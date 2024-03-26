56ffc11eb634474bcd8ea2959cde8b751b2466de
Lucas Felipe A <161534954+LucasFelipeA@users.noreply.github.com>
2024-03-26 09:36:41 -0300

    Alguns exercícios

---
+++ exercícios modulo 1/aula011/ex008.js
@@ -0,0 +1,7 @@
+Print = console.log
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel >60){ // Condição simples!
+    Print(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+Print(`Dirija sempre usando cinto de segurança!`)
---
+++ exercícios modulo 1/aula011/ex009.js
@@ -0,0 +1,8 @@
+Print = console.log
+var país = 'EUA'
+Print(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    Print('Você é Brasileiro!')
+}else{
+    Print('Você é Estrangeiro!')
+}
