adef04b1ad2c6e1dc2384f6268e40866f681f91a
Lucas Felipe A <161534954+LucasFelipeA@users.noreply.github.com>
2024-03-26 12:59:11 -0300

    Aula 12
    Aula 12 do curso de JavaScript

---
+++ exercícios modulo 1/aula12/ex013.js
@@ -0,0 +1,46 @@
+var print = console.log
+var agora = new Date()
+var diaSem = agora.getDay()
+print(diaSem)
+switch (diaSem) {
+    case 0:
+        print('Domingo')
+        break;
+    case 1:
+        print('Segunda')
+        break;
+    case 2:
+        print('Terça')
+        break;
+    case 3:
+        print('Quarta')
+        break;
+    case 4:
+        print('Quinta')
+        break;
+    case 5:
+        print('Sexta')
+        break;
+    case 6:
+        print('Sábado')
+        break;
+    default:
+        print('[ERRO]')
+        break;
+}
+
+var num = 9
+switch(num){
+    case 3:
+    console.log('O número é 3')
+      break
+    case 6:      console.log('O número é 6')
+break
+    case 9:
+      console.log('O número é 9')
+      break
+    default:
+      console.log('O número não está na lista')
+break
+}
+new File('.txt','Abc')
